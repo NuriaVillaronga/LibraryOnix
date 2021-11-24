7c454bc17c53bbfa3afd8bcca09932be01e6eb82
Nuria <nuria.villaronga@xercode.com>
2021-11-24 15:10:01 +0100

    upgrade PublishingDate, Date, dateformat

--- src/Date.php
+++ src/Date.php
@@ -6,20 +6,28 @@ use SimpleXMLElement;
 
 class Date
 {
-
     public ?dateformat $dateformat; // (0,1)
 
     public DateTime $contents;
 
-    public function __construct(SimpleXMLElement $nodeDate)
+    public function __construct(SimpleXMLElement $nodoPublishingDate)
     {
+        $nodeDate = $nodoPublishingDate->Date;
+
         if (isset($nodeDate['dateformat']) == true) {
             $this->dateformat = new dateformat($nodeDate['dateformat']);
         } else {
-            $this->dateformat = null;
+            $this->dateformat = "Ymd";
+        }
+
+        if (isset($nodoPublishingDate->DateFormat) == true) {
+            $this->dateformat = new dateformat($nodoPublishingDate->DateFormat);
+        } else {
+            $this->dateFormat = "Ymd";
         }
 
         $this->contents = DateTime::createFromFormat($this->dateformat, $nodeDate);
+
     }
 }
 
--- src/dateformat.php
+++ src/dateformat.php
@@ -12,5 +12,65 @@ class dateformat
     public function __construct(SimpleXMLElement $nodeAttributeDateformat_nodeDateformat)
     {
         $this->contents = (string) ($nodeAttributeDateformat_nodeDateformat);
+
+        if ($this->contents == "00"){
+            $this->contents = "Ymd"; //AAAAMMDD
+        }
+        else if ($this->contents == "01"){
+            $this->contents = "Ym"; //AAAAMM
+        }
+        else if ($this->contents == "02"){
+            $this->contents = "oW"; //AAAASS siendo S numero de la semana del año (o)
+        }
+        else if ($this->contents == "05"){
+            $this->contents = "Y"; //AAAA
+        }
+        else if ($this->contents == "06"){
+            $this->contents = "YmdYmd"; //AAAAMMDDAAAAMMDD
+        }
+        else if ($this->contents == "07"){
+            $this->contents = "YmYm"; //AAAAMMAAAAMM
+        }
+        else if ($this->contents == "08"){
+            $this->contents = "oWoW"; //formato AAAASSAAAASS
+        }
+        else if ($this->contents == "11"){
+            $this->contents = "YY"; //AAAAAAAA
+        }
+        else if ($this->contents == "13"){
+            $this->contents = "YmdHi"; //YYYYMMDDThhmm
+        }
+        else if ($this->contents == "14"){
+            $this->contents = "YmdHis"; //YYYYMMDDThhmmss
+        }
+        else if ($this->contents == "20"){
+            $this->contents = "Ymd"; //YYYYMMDD (Calendario Hijri)
+        }
+        else if ($this->contents == "21"){
+            $this->contents = "Ym"; //YYYYMM (Calendario Hijri)
+        }
+        else if ($this->contents == "25"){
+            $this->contents = "Y"; //YYYY (Calendario Hijri)
+        }
+        /*
+        if ($this->contents == "03"){
+            $this->contents = ""; //AAAAT -> trimestre ( T = 1,2,3,4)
+        }
+        if ($this->contents == "04"){
+            $this->contents = ""; //AAAAE -> estación (E = 1, 2, 3, 4, siendo 1 = primavera)
+        }
+        if ($this->contents == "09"){
+            $this->contents = ""; //formato AAAATAAAAT
+        }
+        if ($this->contents == "10"){
+            $this->contents = ""; //AAAAEAAAAE
+        }
+        else if ($this->contents == "12"){
+            $this->contents = ""; //Texto. Para fechas complejas, aproximadas o inciertas, o para fechas antes de la era común (AEC o a.C.)
+        }
+        else if ($this->contents == "32"){
+            $this->contents = ""; //Cadena de texto (H)
+        }
+        */
     }
 }
