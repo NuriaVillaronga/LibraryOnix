aadb7dd9fd47397751ff1ca4e9925f3ac07f90a9
Nuria <nuria.villaronga@xercode.com>
2022-02-18 10:04:40 +0100

    changes on publishing date

--- src/Date.php
+++ src/Date.php
@@ -27,16 +27,16 @@ class Date
     {
         $this->contents = (string)($nodePublishingDate->Date);
 
-        if (isset($nodePublishingDate->Date['dateformat']) == true && isset($nodePublishingDate->DateFormat) == false) {
-            $this->dateformat = new dateformatAttr($nodePublishingDate->Date['dateformat']);
-            $this->dateFormat = null;
-            $this->getDateValue($this->dateformat);
-        } 
         if (isset($nodePublishingDate->DateFormat) == true && isset($nodePublishingDate->Date['dateformat']) == false) {
             $this->dateFormat = new DateFormatNode($nodePublishingDate->DateFormat);
             $this->dateformat = null;
             $this->getDateValue($this->dateFormat);
         }
+        if (isset($nodePublishingDate->Date['dateformat']) == true && isset($nodePublishingDate->DateFormat) == false) {
+            $this->dateformat = new dateformatAttr($nodePublishingDate->Date['dateformat']);
+            $this->dateFormat = null;
+            $this->getDateValue($this->dateformat);
+        } 
         if (isset($nodePublishingDate->DateFormat) == false && isset($nodePublishingDate->Date['dateformat']) == false) {
             $this->dateFormat = null;
             $this->dateformat = new dateformatAttr();
